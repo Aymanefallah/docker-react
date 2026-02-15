e273e8e0bae10a1c89b808ca1f81958da75c8bdb
aymane fallah <ayfalmane@gmail.com>
2026-02-15 13:36:08 +0000

    second test qws

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-some changes        </a>
+          Learn React
+        </a>
       </header>
     </div>
   );
